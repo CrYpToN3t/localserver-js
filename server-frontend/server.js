d2ba74e43131e20ffff5b2eff5836b15334641ad
Jamal Guseynov <152898263+CrYpToN3t@users.noreply.github.com>
2023-12-04 19:43:22 +0400

    Add files via upload

---
+++ server-frontend/server.js
@@ -0,0 +1,19 @@
+alert('Server Started!');
+
+const http = require('http')
+const fs = require('fs');
+const { Http2ServerRequest } = require('http2');
+
+const server = http.createServer(function (request, response) {
+    console.log(request.method, request.url);
+    if (request.url == '/style.css') {
+        const css = fs.readFileSync('/style.css', 'utf8');
+        response.end(css);
+    } else {
+        const html = fs.readFileSync('index.html', 'utf8');
+        response.end(html);
+    }
+});
+
+server.listen(3000);
+console.log('Server started!'); 
